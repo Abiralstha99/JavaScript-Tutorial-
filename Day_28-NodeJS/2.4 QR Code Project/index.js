64fe3dc2bf53cf1133c31e3b05010cbba62224f1
Abiral Shrestha <abiralstha333@gmail.com>
2025-01-11 18:17:11 -0500

    Completed QR project

--- Day_28-NodeJS/2.4 QR Code Project/index.js
+++ Day_28-NodeJS/2.4 QR Code Project/index.js
@@ -5,6 +5,31 @@
 */
 
 import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs"
+
 inquirer.prompt([
-    
+    {message: "Enter the url", 
+     name : "URL"  
+    }
 ])
+.then((answers) => {
+    // Use user feedback for... whatever!!
+    console.log(answers);
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr.png'));
+
+    fs.writeFile('qr.txt', url, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      }); 
+
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
